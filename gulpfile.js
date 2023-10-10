9559b6a05a45d43dca367afad772c6f0bcf8c57e
Tarcio Teles <tarcioapps7@gmail.com>
2023-10-09 22:12:30 -0300

    add

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
 const sass = require('gulp-sass')(require('sass'));
 
 function styles () {
-    return gulp.src('./src/styles/*.scss')
+    return gulp.src('./src/styles/**/*')
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./dist/css')); // Corrigido o destino para um diretório "css"
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src('./images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images')); // Corrigido o destino para um diretório "css"
+}
+
+
+exports.default = gulp.parallel(styles,images);
+
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel('styles'))
 }
